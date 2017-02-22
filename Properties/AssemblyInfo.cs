1a08b7c7ab1f6181c03af452b202bcb80a653b7b
Juan C. Luna H. <jcluna.oficial@gmail.com>
2017-02-22 16:47:39 -0600

    Creación del Proyecto

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UBlowfish")]
+[assembly: AssemblyDescription("Implementación del algoritmo Blowfish con BCrypt")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Jcluna")]
+[assembly: AssemblyCopyright("Copyright 2017 - Juan C. Luna H.")]
+[assembly: AssemblyVersion("2017.02.22.1641")]
+[assembly: AssemblyFileVersion("2017.02.22.1641")]
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("CED4F1A9-5783-4FB9-8096-0773DDC3FCB2")]
---
+++ UBlowfish.cs
@@ -0,0 +1,9 @@
+﻿namespace JclunaOficial
+{
+    /// <summary>
+    /// Implementación del algoritmo Blowfish con BCrypt
+    /// </summary>
+    public static class UBlowfish
+    {
+    }
+}
